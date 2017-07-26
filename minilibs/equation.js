c3e6fe2fd62ac307dda16f892b83f4c00d81a1da
TemporalFuzz <vanxmax@gmail.com>
2017-07-25 23:56:13 -0700

    Create equation.js

---
+++ minilibs/equation.js
@@ -0,0 +1,21 @@
+/*
+  This is just a simple Equation toolkit
+  However, it has several important uses
+  For example, it is useful when calculating reflections
+  
+  It finds simple things like slopes and y-intercepts
+*/
+var Equation = {
+  M: function(a, b) {
+    return (a.y - b.y)/(a.x - b.x);
+  },
+  B: function(a, b) {
+    return  a.y - M(a, b) * a.x;   
+  },
+  PM: function(a, b) {
+    return (b.x - a.x)/(a.y - b.y);
+  },
+  TB: function(m, a) {
+    return a.y - (m * a.x);
+  }
+};
