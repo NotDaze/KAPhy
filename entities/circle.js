797eb341a6e02cec9cb37a77bfa041875ad480e7
TemporalFuzz <vanxmax@gmail.com>
2017-07-26 00:20:38 -0700

    Create circle.js

---
+++ entities/circle.js
@@ -0,0 +1,185 @@
+Physics.Circle = function(config) {
+  /** Position and Velocity **/
+  this.pos = config.pos || new Vector2(config.x, config.y);
+  this.vel = config.vel || new Vector2();
+  
+  /** Bounciness **/
+  this.bcf = config.bcf || 0.75;
+  
+  /** Radius **/
+  this.rad = config.rad || 15;
+  
+  /** Array of Constraints **/
+  this.constraints = [];
+  
+  /** Physical Sleep **/
+  this.asleep = false;
+  
+  /** Array of Position Adjustments **/
+  this.posAdjustments = [];
+  
+  /** Mass **/
+  this.mass = config.mass || 1;
+  
+  /** Fixed or Not **/
+  this.fixed = config.fixed || false;
+  
+  /** Variables Used When Submerged**/
+  this.buoyancyForce = 0;
+  this.waterDrag = 1;
+};
+Physics.Circle.prototype.update = function() {
+  if(this.fixed) {
+    this.asleep = false;
+    this.vel = [0, 0];
+    return;
+  }
+  
+  if(vectMag(this.vel) > Physics.sleepThreshold || this.buoyancyForce > 0) {
+    this.asleep = false;
+  }
+  this.pos.add(this.vel)//Add velocity to position
+  
+  this.vel.y += Physics.gravityForce - this.buoyancyForce;//Gravity
+  
+  this.vel.mult(this.waterDrag * (this.asleep ? Physics.asleepAirResistance : Physics.airResistance));//Air resistance
+  
+  this.buoyancyForce = 0;
+  this.waterDrag = 1;
+  
+  this.manageAdjustments();
+};
+Physics.Circle.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = "#FF0000";
+  ctx.ellipse(this.pos[0] * canvas.width/600, this.pos[1] * canvas.height/500, this.rad * canvas.width/600, this.rad * canvas.height/500, 0, 0, 6.279);
+  ctx.fill();
+};
+Physics.Circle.prototype.display = function() {
+  if(this.move) {
+    this.move();
+  }
+  this.update();
+  this.draw();
+};
+Physics.Circle.prototype.moveTowards = function(target, force) {
+  this.applyForce(Vector2.mult(Vector2.normalize(Vector2.sub(target, this.pos)), force));
+};
+Physics.Circle.prototype.moveFrom = function(target, force) {
+  this.applyForce(Vector2.mult(Vector2.normalize(Vector2.sub(this.pos, target)), force));
+};
+Physics.Circle.prototype.manageAdjustments = function() {
+  if(this.posAdjustments.length === 0) {
+    return;
+  }
+  
+  var adjustmentAvg = [0, 0];
+  for(var i = 0; i < this.posAdjustments.length; i++) {
+    adjustmentAvg.add(this.posAdjustments[i]);
+  }
+  this.pos = Vector2.lerp(this.pos, Vector2d.div(adjustmentAvg, this.posAdjustments.length), Physics.constraintAdjustment);
+  
+  this.posAdjustments = [];
+};
+Physics.Circle.prototype.trySleep = function() {
+  if(Vector2.magSq(this.vel) < Physics.sleepThreshold * Physics.sleepThreshold) {
+    this.asleep = true;
+  }
+};
+Physics.Circle.prototype.collideLine = function(line) {
+  if(line.one[0] === line.two[0] ||
+     line.one[1] === line.two[1]) { return; }
+  
+  if(!circleCollidingLine(line.one, line.two, this.pos, this.rad + line.rad)) {
+    return;
+  }
+  if(!intersecting(this.pos, Vector2.reflect(this.pos, line.one, line.two), line.one, line.two)) {
+    var n = (Vector2.dist(this.pos, line.one) < Vector2.dist(this.pos, line.two)) ? line.one : line.two;
+    this.vel = Vector2.mult(Vector2.sub(Vector2.reflect(Vector2.sub(this.pos, this.vel), n, this.pos), this.pos), line.bcf * this.bcf);
+    this.pos = Vector2.sub(n, Vector2d.mult(Vector2.normalize(Vector2.sub(n, this.pos)), this.rad + line.rad));
+  }
+  else{
+    var n = intersection(this.pos, Vector2.reflect(this.pos, line.one, line.two), line.one, line.two);
+    this.vel = Vector2.mult(Vector2.sub(n, Vector2.reflect(Vector2.sub(n, this.vel), n, Vector2.add(n, [1, PM(line.one, line.two)]))), -line.bcf * this.bcf);
+    this.pos = Vector2.add(n, Vector2.mult(Vector2.norm([1, PM(line.one, line.two)]), (this.rad + line.rad) * (this.pos[1] > n[1] ? -1 : 1) * (line.one[0] > line.two[0] ? -1 : 1) * (line.one[1] > line.two[1] ? -1 : 1)));
+  }
+  this.trySleep();
+};
+Physics.Circle.prototype.collideStaticDynamic = function(that) {
+  that.pos = Vector2.lerp(
+    that.pos, 
+    Vector2.add(this.pos,
+      Vector2.mult(
+        Vector2.normalize(
+          Vector2.sub(that.pos, this.pos)
+        ),
+      this.rad + that.rad)
+    ),
+    0.15
+  );
+  that.vel = Vector2.mult(
+    Vector2.sub(
+      Vector2.reflect(
+        Vector2.sub(that.pos, that.vel),
+      this.pos, that.pos),
+    that.pos),
+  this.bcf * that.bcf);
+  that.pos = Vector2.add(that.pos, that.vel);
+};
+Physics.Circle.prototype.collideDynamicStatic = function(that) {
+  this.pos = Vector2.lerp(
+    this.pos,
+    Vector2.add(that.pos,
+      Vector2.mult(
+        Vector2.normalize(
+          Vector2.sub(this.pos, that.pos)
+        ),
+      this.rad + that.rad)
+    ),
+    0.15
+  );
+  this.vel = Vector2.mult(
+    Vector2.sub(
+      Vector2.reflect(
+        Vector2.sub(this.pos, this.vel),
+      that.pos, this.pos),
+    this.pos), 
+  this.bcf * that.bcf);
+  this.pos = Vector2.add(this.pos, this.vel);
+};
+Physics.Circle.prototype.collideDynamicDynamic = function(that) {
+  var mid = Vector2.mid(this.pos, that.pos);
+  var difference = Vector2.norm(Vector2.sub(this.pos, that.pos));
+  
+  this.pos = Vector2.lerp(this.pos, Vector2.add(mid, Vector2.mult(difference, this.rad + that.rad)), 0.15);
+  that.pos = Vector2.lerp(that.pos, Vector2.sub(mid, Vector2.mult(difference, this.rad + that.rad)), 0.15);
+  
+  var thisComponentOld = Vector2.dot(this.vel, difference);
+  var thatComponentOld = Vector2.dot(that.vel, difference);
+  
+  var thisComponentNew = thatComponentOld * that.mass/this.mass;
+  var thatComponentNew = thisComponentOld * this.mass/that.mass;
+  
+  this.vel = Vector2.add(this.vel, Vector2.mult(difference, (thisComponentNew - thisComponentOld) * this.bcf * that.bcf));
+  that.vel = Vector2.add(that.vel, Vector2.mult(difference, (thatComponentNew - thatComponentOld) * this.bcf * that.bcf));
+};
+Physics.Circle.prototype.collideCircle = function(that) {
+  if(this.fixed && that.fixed) {
+    return;
+  }
+  if(Vector2.magSq(Vector2.sub(this.pos, that.pos)) > ((this.rad + that.rad) * (this.rad + that.rad))) {
+    return;
+  }
+  if(this.fixed) {
+    this.collideStaticDynamic(that);
+    return;
+  }
+  if(that.fixed) {
+    this.collideDynamicStatic(that);
+    return;
+  }
+  this.collideDynamicDynamic(that);
+};
+Physics.Circle.prototype.applyForce = function(velChange) {
+  this.vel = Vector2.add(this.vel, Vector2.div(velChange, this.mass));
+};
