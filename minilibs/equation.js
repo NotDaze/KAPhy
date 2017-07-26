5bc5d2c075c3c1aad6427272f0d8718e0ec47528
TemporalFuzz <vanxmax@gmail.com>
2017-07-26 10:12:42 -0700

    Update equation.js

--- minilibs/equation.js
+++ minilibs/equation.js
@@ -10,7 +10,7 @@ var Equation = {
     return (a.y - b.y)/(a.x - b.x);
   },
   B: function(a, b) {
-    return  a.y - M(a, b) * a.x;   
+    return  a.y - (a.y - b.y)/(a.x - b.x) * a.x;   
   },
   PM: function(a, b) {
     return (b.x - a.x)/(a.y - b.y);
