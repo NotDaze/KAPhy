c1d34f49d5aa0a717633d1c8f058d3abf0aa70cf
TemporalFuzz <vanxmax@gmail.com>
2017-07-25 23:25:36 -0700

    Create init.js

---
+++ init.js
@@ -0,0 +1,11 @@
+const Physics = {
+  airResistance: 0.99,//The velocity of everything is multiplied by this number each frame.
+  airResistanceSleeping: 0.9,//The air resistance of something that is "asleep".
+  
+  gravityForce: 0.7,//The magnitude of gravity
+  
+  constraintCompensation: 0.2,//The intensity of rigid constraints' compensation for oscillation.
+  constraintAdjustment: 0.3,//How much rigid constraints adjust the 
+  
+  sleepThreshold: 2,//Maximum velocity at which any circle can be "asleep".
+};
