28a0d7e5e47c6741a5c9779dd8a6aa15626ae5e0
TemporalFuzz <vanxmax@gmail.com>
2017-07-26 21:16:34 -0700

    Create extender.js

---
+++ physics/entities/constraints/extender.js
@@ -0,0 +1,11 @@
+Physics.Extender = function(config) {
+  Physics.Constraint.call(this, config);
+};
+Physics.Extender.prototype = Object.create(Physics.Constraint.prototype);
+Physics.Extender.prototype.update = function() {
+  var distance = Vector2.dist(this.one.pos, this.two.pos);
+  if(distance < this.length) {
+    this.springEffect();
+    this.forceCompensate();
+  }
+};
