cef094bce57c094290e654b60e9f81df103f94e6
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 11:18:23 -0700

    Update cord.js

--- physics/entities/constraints/cord.js
+++ physics/entities/constraints/cord.js
@@ -1,11 +1,13 @@
-Physics.Cord = function(config) {
-  Physics.Constraint.call(this, config);
-};
-Physics.Cord.prototype = Object.create(Physics.Constraint.prototype);
-Physics.Cord.prototype.update = function() {
-  var distance = Vector2.dist(this.one.pos, this.two.pos);
-  if(distance > this.length) {
-    this.springEffect();
-    this.forceCompensate();
-  }
-};
+if (!Physics.Cord) {
+  Physics.Cord = function(config) {
+    Physics.Constraint.call(this, config);
+  };
+  Physics.Cord.prototype = Object.create(Physics.Constraint.prototype);
+  Physics.Cord.prototype.update = function() {
+    var distance = Vector2.dist(this.one.pos, this.two.pos);
+    if (distance > this.length) {
+      this.springEffect();
+      this.forceCompensate();
+    }
+  };
+}
