26994333b312167d39cdb16f61b91e9398875083
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 16:12:58 -0700

    Create images.js

---
+++ gfx/images.js
@@ -0,0 +1,8 @@
+if(!Images) {
+  var Images = {
+    aqualineSeed: "https://www.kasandbox.org/programming-images/avatars/aqualine-seed.png",
+    aqualineSeedling: "https://www.kasandbox.org/programming-images/avatars/aqualine-seedling.png",
+    aqualineSapling: "https://www.kasandbox.org/programming-images/avatars/aqualine-sapling.png",
+    aqualineTree: "https://www.kasandbox.org/programming-images/avatars/aqualine-tree.png"
+  };
+}
