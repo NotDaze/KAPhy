c1afc1a8ac6f13d5ecbee5d7a85ff6ce7169fc34
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 10:42:16 -0700

    Update init.js

--- physics/init.js
+++ physics/init.js
@@ -1,13 +1,15 @@
-const Physics = {
-  airResistance: 0.99,//The velocity of everything is multiplied by this number each frame.
-  airResistanceSleeping: 0.9,//The air resistance of something that is "asleep".
+if(!Physics) {
+  var Physics = {
+    airResistance: 0.99,//The velocity of everything is multiplied by this number each frame.
+    airResistanceSleeping: 0.9,//The air resistance of something that is "asleep".
   
-  gravityForce: 0.7,//The magnitude of gravity
+    gravityForce: 0.7,//The magnitude of gravity
   
-  constraintCompensation: 0.2,//The intensity of rigid constraints' compensation for oscillation.
-  constraintAdjustment: 0.3,//How much rigid constraints adjust the 
+    constraintCompensation: 0.2,//The intensity of rigid constraints' compensation for oscillation.
+    constraintAdjustment: 0.3,//How much rigid constraints adjust the 
   
-  sleepThreshold: 2,//Maximum velocity at which any circle can be "asleep".
+    sleepThreshold: 2,//Maximum velocity at which any circle can be "asleep".
   
-  circleAdjustment: 0.3//The intensity of the position adjustment when circles collide
-};
+    circleAdjustment: 0.3//The intensity of the position adjustment when circles collide
+  };
+}
