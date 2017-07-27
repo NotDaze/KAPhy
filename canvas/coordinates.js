ecadc3f4746c4a788e674d27b32c2ae42741f55f
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 11:23:38 -0700

    Update coordinates.js

--- canvas/coordinates.js
+++ canvas/coordinates.js
@@ -1,6 +1,10 @@
-Canvas.getX = function(x) {
-  return x * Canvas.element.width/Canvas.relWidth;
-};
-Canvas.getY = function(y) {
-  return y * Canvas.element.height/Canvas.relHeight;
-};
+if(!Canvas.getX) {
+  Canvas.getX = function(x) {
+    return x * Canvas.element.width/Canvas.relWidth;
+  };
+}
+if(!Canvas.getY) {
+  Canvas.getY = function(y) {
+    return y * Canvas.element.height/Canvas.relHeight;
+  };
+}
