b1104626f5c411fe2b036bfa9d21cb9672252bdc
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 13:53:16 -0700

    Update fill.js
    Some slight exploits to prevent the context's fillstyle from converting to hex.

--- draw/colors/fill.js
+++ draw/colors/fill.js
@@ -6,11 +6,11 @@ if(!Draw.fill) {
     }
     
     switch(arguments.length) {
-      case 0: Canvas.context.fillStyle = "rgba(255, 255, 255, 1)"; break;
-      case 1: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[0] + ", " + arguments[0] + ", 1.0)"; break;
-      case 2: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[0] + ", " + arguments[0] + ", " + arguments[1]/255 + ")"; break;
-      case 3: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ", 1.0)"; break;
-      case 4: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ", " + arguments[3]/255 + ")";
+      case 0: Canvas.context.fillStyle = "rgba(255, 255, 255, 0.999)"; break;
+      case 1: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[0] + ", " + arguments[0] + ", 0.999)"; break;
+      case 2: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[0] + ", " + arguments[0] + ", " + (arguments[1]/255 - 0.001) + ")"; break;
+      case 3: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ", 0.999)"; break;
+      case 4: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ", " + (arguments[3]/255 - 0.001) + ")";
     }
   };
 }
