521ec1a82c0e991dac273b39884cc855145fa224
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 11:19:13 -0700

    Update rod.js

--- physics/entities/constraints/rod.js
+++ physics/entities/constraints/rod.js
@@ -1,8 +1,10 @@
-Physics.Rod = function(config) {
-  Physics.Constraint.call(this, config);
-};
-Physics.Rod.prototype = Object.create(Physics.Constraint.prototype);
-Physics.Rod.prototype.update = function() {
-  this.springEffect();
-  this.forceCompensate();
-};
+if(!Physics.Rod) {
+  Physics.Rod = function(config) {
+    Physics.Constraint.call(this, config);
+  };
+  Physics.Rod.prototype = Object.create(Physics.Constraint.prototype);
+  Physics.Rod.prototype.update = function() {
+    this.springEffect();
+    this.forceCompensate();
+  };
+}
