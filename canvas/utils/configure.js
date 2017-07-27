739c899c0cdeb9f517705eca76cf2f4531df6266
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 11:23:03 -0700

    Update and rename canvas/configure.js to canvas/utils/configure.js

---
+++ canvas/utils/configure.js
@@ -0,0 +1,16 @@
+if (!Canvas.configure) {
+  Canvas.configure = function(canvasElement) {
+    if (this.configured) {
+      console.warn("KAPhy warning - Attempted to reconfigure canvas.");
+      return;
+    }
+
+    Canvas.element = canvasElement;
+    Canvas.configured = true;
+
+    Canvas.relWidth = canvasElement.width;
+    Canvas.relHeight = canvasElement.height;
+
+    Canvas.context = canvasElement.getContext("2d");
+  };
+}
