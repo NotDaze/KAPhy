1246a3993f8c295afdfbb1c80bc4e0a4492d0ded
TemporalFuzz <vanxmax@gmail.com>
2017-07-26 22:11:15 -0700

    Update rect.js

--- draw/shapes/rect.js
+++ draw/shapes/rect.js
@@ -6,8 +6,8 @@ Draw.rect = function(x, y, w, h) {
     case CORNERS: Canvas.context.fillRect(x, y, w - x, h - y);
                  Canvas.context.strokeRect(x, y, w - x, h - y); break;
     case RADIUS: Canvas.context.fillRect(x + w/2, y + h/2, w * 2, h * 2);
-                 Canvas.context.strokeRect(x, y, w, h); break;
-    case CENTER: Canvas.context.fillRect(x, y, w, h);
+                 Canvas.context.strokeRect(x + w/2, y + h/2, w * 2, h * 2); break;
+    case CENTER: Canvas.context.fillRect(x + w/2, y + h/2, w, h);
                  Canvas.context.strokeRect(x + w/2, y + h/2, w, h); break;
   }
   Canvas.context.fill();
