48da56c3d7092c05e9db69d7f607e59bef22a906
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 11:16:51 -0700

    Update bungee.js

--- physics/entities/constraints/bungee.js
+++ physics/entities/constraints/bungee.js
@@ -1,10 +1,12 @@
-Physics.Bungee = function(config) {
-  Physics.Constraint.call(this, config);
-};
-Physics.Bungee.prototype = Object.create(Physics.Constraint.prototype);
-Physics.Bungee.prototype.update = function() {
-  var distance = Vector2.dist(this.one.pos, this.two.pos);
-  if(distance > this.length) {
-    this.springEffect();
-  }
-};
+if (!Physics.Bungee) {
+  Physics.Bungee = function(config) {
+    Physics.Constraint.call(this, config);
+  };
+  Physics.Bungee.prototype = Object.create(Physics.Constraint.prototype);
+  Physics.Bungee.prototype.update = function() {
+    var distance = Vector2.dist(this.one.pos, this.two.pos);
+    if (distance > this.length) {
+      this.springEffect();
+    }
+  };
+}
