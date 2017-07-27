c4c07ef040944908d0b99b10b00088f861af39d0
TemporalFuzz <vanxmax@gmail.com>
2017-07-26 23:08:00 -0700

    Update resize.js
    Bugfixes and improvements

--- canvas/resize.js
+++ canvas/resize.js
@@ -1,12 +1,15 @@
 Canvas.resize = function(relWidth, relHeight) {
   console.log("Canvas Resized To Relative Dimensions - " + relWidth + ", " + relHeight);
   
+  Canvas.relWidth = relWidth;
+  Canvas.relHeight = relHeight;
+  
   if(window.innerWidth/window.innerHeight < relWidth/relHeight) {
     Canvas.element.width  = window.innerWidth;
     Canvas.element.height = relHeight/relWidth * window.innerWidth;
   }
   else {
-    Canvas.width  = relWidth/relHeight * window.innerHeight;
-    Canvas.height = window.innerHeight;
+    Canvas.element.width  = relWidth/relHeight * window.innerHeight;
+    Canvas.element.height = window.innerHeight;
   }
 };
