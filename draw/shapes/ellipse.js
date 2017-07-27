66db73113f39768eba6da9421dee5b1faa23edfa
TemporalFuzz <vanxmax@gmail.com>
2017-07-26 23:27:17 -0700

    Update ellipse.js
    Fixed more careless errors

--- draw/shapes/ellipse.js
+++ draw/shapes/ellipse.js
@@ -1,10 +1,10 @@
-Draw.rect = function(x, y, w, h) {
+Draw.ellipse = function(x, y, w, h) {
   Canvas.context.beginPath();
-  switch(Draw.currentRectMode) {
-    case CENTER: Canvas.ellipse(x, y, w, h, 0, 0, 6.3) break;
-    case CORNER: Canvas.ellipse(x - w/2, y - h/2, w, h, 0, 0, 6.3) break;
-    case CORNERS: Canvas.ellipse(x - w/2, y - h/2, w, h, 0, 0, 6.3) break;
-    case RADIUS: Canvas.ellipse(x, y, w * 2, h * 2, 0, 0, 6.3) break;
+  switch(Draw.currentEllipseMode) {
+    case CENTER: Canvas.context.ellipse(x, y, w, h, 0, 0, 6.3); break;
+    case CORNER: Canvas.context.ellipse(x - w/2, y - h/2, w, h, 0, 0, 6.3); break;
+    case CORNERS: Canvas.context.ellipse(x - w/2, y - h/2, w, h, 0, 0, 6.3); break;
+    case RADIUS: Canvas.context.ellipse(x, y, w * 2, h * 2, 0, 0, 6.3); break;
   }
   Canvas.context.fill();
   Canvas.context.stroke();
