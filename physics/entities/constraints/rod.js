02f52b82d1497d0af4f1f1333afd86ec9ef4ed25
TemporalFuzz <vanxmax@gmail.com>
2017-07-26 21:16:54 -0700

    Create rod.js

---
+++ physics/entities/constraints/rod.js
@@ -0,0 +1,8 @@
+Physics.Rod = function(config) {
+  Physics.Constraint.call(this, config);
+};
+Physics.Rod.prototype = Object.create(Physics.Constraint.prototype);
+Physics.Rod.prototype.update = function() {
+  this.springEffect();
+  this.forceCompensate();
+};
