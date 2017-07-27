8fcaf0d84792748eec75dc6680ecdcd4492b1ce0
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 14:24:13 -0700

    Create getstrokeweight.js

---
+++ draw/colors/getstrokeweight.js
@@ -0,0 +1,10 @@
+if(!Draw.getStrokeWeight) {
+  Draw.getStrokeWeight = function() {
+    if(!Canvas.configured) {
+      console.warn("KAPhy Warning - You must use Canvas.configure(); before you can draw!");
+      return;
+    }
+    
+    return Canvas.context.lineWidth;
+  };
+}
