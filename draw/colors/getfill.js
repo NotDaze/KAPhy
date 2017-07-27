028d9bbae8c787f4a09a2e4c1eea08b43de61497
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 13:46:08 -0700

    Update getfill.js

--- draw/colors/getfill.js
+++ draw/colors/getfill.js
@@ -7,18 +7,19 @@ if(!Draw.getFill) {
     
     var fillStyle = Canvas.context.fillStyle.split(", ");
     fillStyle[0] = fillStyle[0].split("");
-    fillStyle[0].splice(0, 4);
-    fillStyle[0] = parseFloat(fillStyle.join(""));
+    fillStyle[0].splice(0, 5);
+    fillStyle[0] = parseFloat(fillStyle[0].join(""));
     fillStyle[1] = parseFloat(fillStyle[1]);
-    fillStyle[2] = fillStyle[2].split("");
-    fillStyle[2].pop();
-    fillStyle[2] = parseFloat(fillStyle[2].join(""));
+    fillStyle[2] = parseFloat(fillStyle[2]);
+    fillStyle[3] = fillStyle[3].split("");
+    fillStyle[3].pop();
+    fillStyle[3] = parseFloat(fillStyle[3].join(""));
     
     return {
       r: fillStyle[0],
       g: fillStyle[1], 
       b: fillStyle[2],
-      a: Canvas.context.globalAlpha * 255
+      a: fillStyle[3] * 255
     };
   };
 }
