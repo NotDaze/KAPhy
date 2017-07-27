f8f05533ee5c2d1edb54e06befbcaf5f0e1b323e
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 13:25:17 -0700

    Update rect.js

--- draw/shapes/rect.js
+++ draw/shapes/rect.js
@@ -1,5 +1,10 @@
 if (!Draw.rect) {
   Draw.rect = function(x, y, w, h, tl, tr, br, bl) {
+    if(!Canvas.configured) {
+      console.warn("KAPhy Warning - You must use Canvas.configure(); before you can draw!");
+      return;
+    }
+    
     var realX = x,
       realY = y,
       realW = w,
