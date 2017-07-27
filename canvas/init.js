31b1701ab02520788982c0e9a376b877365bb1f4
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 11:22:06 -0700

    Update init.js

--- canvas/init.js
+++ canvas/init.js
@@ -1,7 +1,9 @@
-const Canvas = {
-  configured: false,
-  relWidth: null,
-  relHeight: null,
-  element: null,
-  context: null
-};
+if(!Canvas) {
+  var Canvas = {
+    configured: false,
+    relWidth: null,
+    relHeight: null,
+    element: null,
+    context: null
+  };
+}
