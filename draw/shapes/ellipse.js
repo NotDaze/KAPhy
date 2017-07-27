acf3601b7a67ded8690c5e1751312cd3f44eebb8
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 11:34:42 -0700

    Update ellipse.js

--- draw/shapes/ellipse.js
+++ draw/shapes/ellipse.js
@@ -1,12 +1,34 @@
 if(!Draw.ellipse) {
   Draw.ellipse = function(x, y, w, h) {
     Canvas.context.beginPath();
+    
+    var realX = x,
+        realY = y,
+        realW = w,
+        realH = h;
+    
     switch(Draw.currentEllipseMode) {
-      case CENTER: Canvas.context.ellipse(x, y, w, h, 0, 0, 6.3); break;
-      case CORNER: Canvas.context.ellipse(x - w/2, y - h/2, w, h, 0, 0, 6.3); break;
-      case CORNERS: Canvas.context.ellipse(x - w/2, y - h/2, w, h, 0, 0, 6.3); break;
-      case RADIUS: Canvas.context.ellipse(x, y, w * 2, h * 2, 0, 0, 6.3); break;
+      case CORNER: 
+        realX = x - w/2;
+        realY = y - h/2; break;
+      case CORNERS:
+        realX = (x + w)/2;
+        realY = (y + h)/2;
+        realW = w - x;
+        realH = h - y; break;
+      case RADIUS: 
+        realW = w * 2;
+        realH = h * 2; break;
     }
+    
+    if(Canvas.configured) {
+      realX = Canvas.getX(realX);
+      realY = Canvas.getY(realY);
+      realW = Canvas.getX(realW);
+      realH = Canvas.getY(realH);
+    }
+    
+    Canvas.context.ellipse(realX, realY, realW, realH, 0, 0, 6.3);
     Canvas.context.fill();
     Canvas.context.stroke();
   };
