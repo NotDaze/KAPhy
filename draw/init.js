fc4211877e91cdafb292b1a0ba5a4cc00a8a3dff
TemporalFuzz <vanxmax@gmail.com>
2017-07-26 23:10:16 -0700

    Update init.js

--- draw/init.js
+++ draw/init.js
@@ -1,1 +1,9 @@
-var Draw = {};
+const CENTER = "CENTER";
+const CORNER = "CORNER";
+const RADIUS = "RADIUS";
+const CORNERS = "CORNERS";
+
+var Draw = {
+  currentRectMode: "CORNER",
+  currentEllipseMode: "CENTER",
+};
