0f9c1fd64e543c09103301696656c90cc9cffa26
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 13:44:12 -0700

    Update fill.js

--- draw/colors/fill.js
+++ draw/colors/fill.js
@@ -6,10 +6,10 @@ if(!Draw.fill) {
     }
     
     switch(arguments.length) {
-      case 0: Canvas.context.fillStyle = "#FFFFFF"; break;
-      case 1: Canvas.context.fillStyle = "rgb(" + arguments[0] + ", " + arguments[0] + ", " + arguments[0] + ")"; break;
+      case 0: Canvas.context.fillStyle = "rgba(255, 255, 255, 1)"; break;
+      case 1: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[0] + ", " + arguments[0] + ", 1.0)"; break;
       case 2: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[0] + ", " + arguments[0] + ", " + arguments[1]/255 + ")"; break;
-      case 3: Canvas.context.fillStyle = "rgb(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ")"; break;
+      case 3: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ", 1.0)"; break;
       case 4: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ", " + arguments[3]/255 + ")";
     }
   };
