a32ff0424d2995c1f185c569d24f5191b39552b7
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 11:19:36 -0700

    Update spring.js

--- physics/entities/constraints/spring.js
+++ physics/entities/constraints/spring.js
@@ -1,7 +1,9 @@
-Physics.Spring = function(config) {
-  Physics.Constraint.call(this, config);
-};
-Physics.Spring.prototype = Object.create(Physics.Constraint.prototype);
-Physics.Spring.prototype.update = function() {
-  this.springEffect();
-};
+if(!Physics.Spring) {
+  Physics.Spring = function(config) {
+    Physics.Constraint.call(this, config);
+  };
+  Physics.Spring.prototype = Object.create(Physics.Constraint.prototype);
+  Physics.Spring.prototype.update = function() {
+    this.springEffect();
+  };
+}
