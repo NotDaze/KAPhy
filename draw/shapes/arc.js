75e9454339e39e43d33690770120163313068492
TemporalFuzz <vanxmax@gmail.com>
2017-07-26 23:28:08 -0700

    Update arc.js

--- draw/shapes/arc.js
+++ draw/shapes/arc.js
@@ -1,10 +1,10 @@
-Draw.rect = function(x, y, w, h, start, stop) {
+Draw.arc = function(x, y, w, h, start, stop) {
   Canvas.context.beginPath();
-  switch(Draw.currentRectMode) {
-    case CENTER: Canvas.ellipse(x, y, w, h, 0, start, stop) break;
-    case CORNER: Canvas.ellipse(x - w/2, y - h/2, w, h, 0, start, stop) break;
-    case CORNERS: Canvas.ellipse(x - w/2, y - h/2, w, h, 0, start, stop) break;
-    case RADIUS: Canvas.ellipse(x, y, w * 2, h * 2, 0, start, stop) break;
+  switch(Draw.currentEllipseMode) {
+    case CENTER: Canvas.context.ellipse(x, y, w, h, 0, start, stop); break;
+    case CORNER: Canvas.context.ellipse(x - w/2, y - h/2, w, h, 0, start, stop); break;
+    case CORNERS: Canvas.context.ellipse(x - w/2, y - h/2, w, h, 0, start, stop); break;
+    case RADIUS: Canvas.context.ellipse(x, y, w * 2, h * 2, 0, start, stop); break;
   }
   Canvas.context.fill();
   Canvas.context.stroke();
