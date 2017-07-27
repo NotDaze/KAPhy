0c28848f64aa1bb7c858a8aff1e5cd78bf11e1ab
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 11:17:55 -0700

    Update compressor.js

--- physics/entities/constraints/compressor.js
+++ physics/entities/constraints/compressor.js
@@ -1,10 +1,12 @@
-Physics.Compressor = function(config) {
-  Physics.Constraint.call(this, config);
-};
-Physics.Compressor.prototype = Object.create(Physics.Constraint.prototype);
-Physics.Compressor.prototype.update = function() {
-  var distance = Vector2.dist(this.one.pos, this.two.pos);
-  if(distance < this.length) {
-    this.springEffect();
-  }
-};
+if (!Physics.Compressor) {
+  Physics.Compressor = function(config) {
+    Physics.Constraint.call(this, config);
+  };
+  Physics.Compressor.prototype = Object.create(Physics.Constraint.prototype);
+  Physics.Compressor.prototype.update = function() {
+    var distance = Vector2.dist(this.one.pos, this.two.pos);
+    if (distance < this.length) {
+      this.springEffect();
+    }
+  };
+}
