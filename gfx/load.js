7d0cef4bf8407690aa57e30e6a6f2bbc1ad96d4d
TemporalFuzz <vanxmax@gmail.com>
2017-07-27 16:18:27 -0700

    Update load.js

--- gfx/load.js
+++ gfx/load.js
@@ -1,6 +1,7 @@
 if(!loaded) {
   var Images = {};
   var imagesLoaded = 0;
+  var loaded = true;
   for(var i in ImagesToLoad) {
     var oldVal = ImagesToLoad[i];
     ImagesToLoad[i] = new Image();
