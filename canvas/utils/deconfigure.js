0026dcc71da1b986d9969b32e8329c7e99c0e2ff
TemporalFuzz <vanxmax@gmail.com>
2017-07-28 01:46:07 -0700

    Create deconfigure.js

---
+++ canvas/utils/deconfigure.js
@@ -0,0 +1,16 @@
+if (!Canvas.deconfigure) {
+  Canvas.deconfigure = function(canvasElement) {
+    if (!this.configured) {
+      console.warn("KAPhy warning - Attempted to reconfigure canvas.");
+      return;
+    }
+
+    Canvas.element = null;
+    Canvas.configured = false;
+
+    Canvas.relWidth = null;
+    Canvas.relHeight = null;
+
+    Canvas.context = null;
+  };
+}
