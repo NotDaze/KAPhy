a7d37bba41c445220e69a9f2817276c769df0a61
TemporalFuzz <vanxmax@gmail.com>
2017-07-28 00:33:27 -0700

    Update images.js

--- gfx/images.js
+++ gfx/images.js
@@ -1,5 +1,5 @@
-if(!ImagesToLoad) {
-  var ImagesToLoad = {
+if(!Images) {
+  var Images = {
     aqualineSeed: "https://www.kasandbox.org/programming-images/avatars/aqualine-seed.png",
     aqualineSeedling: "https://www.kasandbox.org/programming-images/avatars/aqualine-seedling.png",
     aqualineSapling: "https://www.kasandbox.org/programming-images/avatars/aqualine-sapling.png",
