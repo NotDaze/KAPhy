1ec8f4d8dd9e9da24d70ad0d56a7f1a3f5048913
TemporalFuzz <vanxmax@gmail.com>
2017-07-28 12:46:26 -0700

    Update fill.js

--- draw/colors/fill.js
+++ draw/colors/fill.js
@@ -1,9 +1,24 @@
 if(!Draw.fill) {
-  Draw.fill = function(r, g, b, a) {
+  /** Draw.fill
+    Draw.fill sets the canvas context's fill color.
+    
+    @import  https://cdn.rawgit.com/TemporalFuzz/KAPhy/edit/master/draw/colors/fill.js
+    @author  TemporalFuzz
+    @version 1.0
+  */
+  Draw.fill = function() {
     if(!Canvas.configured) {
       console.warn("KAPhy Warning - You must use Canvas.configure(); before you can draw!");
       return;
     }
+    
+    /*
+      If no arguments: Defaults to white
+      If one argument: Gray of brightness depending on argument
+      If two arguments: First argument is brightness; second is alpha
+      If three arguments: Typical RGB
+      If four arguments: Typical RGBA
+    */
     
     switch(arguments.length) {
       case 0: Canvas.context.fillStyle = "#FFFFFF"; break;
@@ -11,6 +26,7 @@ if(!Draw.fill) {
       case 2: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[0] + ", " + arguments[0] + ", " + (arguments[1]/255) + ")"; break;
       case 3: Canvas.context.fillStyle = "rgb(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ")"; break;
       case 4: Canvas.context.fillStyle = "rgba(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ", " + (arguments[3]/255) + ")";
+      default: console.warn("KAPhy Warning - Draw.fill() takes 0 - 4 arguments."); break;
     }
   };
 }
