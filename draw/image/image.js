894037e7b27d30951fc5e1785e571bef25365c82
TemporalFuzz <vanxmax@gmail.com>
2017-07-28 12:03:21 -0700

    Update image.js

--- draw/image/image.js
+++ draw/image/image.js
@@ -1,10 +1,14 @@
 if(!Draw.image) {
-  Draw.image = function(image, x, y, w, h) {
+  Draw.image = function(image, x, y, w, h, sx, sy, sw, sh) {
     if(!Canvas.configured) {
       console.warn("KAPhy Warning - You must use Canvas.configure(); before you can draw!");
       return;
     }
     
-    Canvas.context.drawImage(image, x, y, w, h);
+    switch(arguments.length) {
+      case 3: Canvas.context.drawImage(image, x, y); break;
+      case 5: Canvas.context.drawImage(image, x, y, w, h); break;
+      case 9: Canvas.context.drawImage(image, x, y, w, h, sx, sy, sw, sh); break;
+    }
   };
 }
