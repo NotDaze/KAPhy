bd8e36ab906ae9872605654140467a11780a299b
TemporalFuzz <vanxmax@gmail.com>
2017-07-28 00:32:40 -0700

    Update load.js

--- gfx/load.js
+++ gfx/load.js
@@ -1,22 +1,8 @@
-var Images = {},
-    imageCount = 0;
-
-var onFinish = function() {};
-
 if(!loaded) {
-  var imagesLoaded = 0;
   var loaded = true;
-  for(var i in ImagesToLoad) {
-    imageCount++;
-    var oldVal = ImagesToLoad[i];
-    ImagesToLoad[i] = new Image();
-    ImagesToLoad[i].onload = function() {
-      imagesLoaded++;
-      Images[i] = this;
-      if(imagesLoaded === imageCount) {
-        onFinish();
-      }
-    };
-    ImagesToLoad[i].src = oldVal;
+  for(var i in Images) {
+    var oldVal = Images[i];
+    Images[i] = new Image();
+    Images[i].src = oldVal;
   }
 }
