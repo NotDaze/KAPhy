999db3bb731155cdb537e75c4378a083717acabe
TemporalFuzz <vanxmax@gmail.com>
2017-07-29 13:42:46 -0700

    Update getstrokeweight.js

--- draw/colors/getstrokeweight.js
+++ draw/colors/getstrokeweight.js
@@ -1,11 +1,4 @@
-if(!Draw.getStrokeWeight) {
-  /** Draw.getStrokeWeight
-    Draw.getStrokeWeight returns the canvas context's current stroke weight (or line width)
-    
-    @author  TemporalFuzz
-    @version 1.0
-    @returns Number current stroke weight
-  */
+if(!Draw.getStrokeWeight || KAPhy.version !== KAPhy.current) {
   Draw.getStrokeWeight = function() {
     if(!Canvas.configured) {
       console.warn("KAPhy Warning - You must use Canvas.configure(); before you can draw!");
