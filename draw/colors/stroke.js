93b9540c24285406d37631c93608cc5c3c7e934b
TemporalFuzz <vanxmax@gmail.com>
2017-07-28 20:56:43 -0700

    Update stroke.js

--- draw/colors/stroke.js
+++ draw/colors/stroke.js
@@ -5,12 +5,29 @@ if(!Draw.stroke) {
       return;
     }
     
+    /*
+      If no arguments: Defaults to white
+      If one argument: Gray of brightness depending on argument
+      If one argument: Sets color based on the object's r, g, b, and a properties.
+      If two arguments: First argument is brightness; second is alpha
+      If three arguments: Typical RGB
+      If four arguments: Typical RGBA
+    */
+    
+    if(arguments.length >= 1) {
+      if(typeof arguments[0] === "object") {
+        Canvas.context.strokeStyle = "rgba(" + arguments[0].r + ", " + arguments[0].g + ", " + arguments[0].b + ", " + (arguments[0].a/255) + ")";
+        return;
+      }
+    }
+    
     switch(arguments.length) {
       case 0: Canvas.context.strokeStyle = "#FFFFFF"; break;
       case 1: Canvas.context.strokeStyle = "rgb(" + arguments[0] + ", " + arguments[0] + ", " + arguments[0] + ")"; break;
       case 2: Canvas.context.strokeStyle = "rgba(" + arguments[0] + ", " + arguments[0] + ", " + arguments[0] + ", " + (arguments[1]/255) + ")"; break;
       case 3: Canvas.context.strokeStyle = "rgb(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ")"; break;
-      case 4: Canvas.context.strokeStyle = "rgba(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ", " + (arguments[3]/255) + ")";
+      case 4: Canvas.context.strokeStyle = "rgba(" + arguments[0] + ", " + arguments[1] + ", " + arguments[2] + ", " + (arguments[3]/255) + ")"; break;
+      default: console.warn("KAPhy Warning - Draw.stroke() takes 0 - 4 arguments.");
     }
   };
 }
