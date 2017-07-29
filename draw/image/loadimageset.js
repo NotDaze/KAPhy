6e6134a833e421c0258b4aae347bd2f927e6bb92
TemporalFuzz <vanxmax@gmail.com>
2017-07-28 17:36:03 -0700

    Update loadimageset.js

--- draw/image/loadimageset.js
+++ draw/image/loadimageset.js
@@ -1,15 +1,12 @@
 if(!Draw.loadImageSet) {
-  Draw.loadImageSet = function() {
+  Draw.loadImageSet = function(imagesToLoad, onFinish) {
     if(arguments.length === 0) {
       console.warn("KAPhy Warning - Draw.loadImageSet() takes at least 1 argument.");
       return;
     }
     
     var imagesLoaded = 0;
-    var imagesToLoad = arguments;
     var loadedImages = [];
-    imagesToLoad.pop();
-    var onFinish = arguments[arguments.length - 1];
     
     for(var i = 0; i < imagesToLoad.length; i++) {
       Draw.loadImage(imagesToLoad[i], function() {
