51e6fddbcfe3d7ef6a4492772bc58975b3e6bd2f
TemporalFuzz <vanxmax@gmail.com>
2017-07-28 20:59:02 -0700

    Update get.js

--- draw/image/get.js
+++ draw/image/get.js
@@ -1,10 +1,26 @@
 if(!Draw.get) {
   Draw.get = function(x, y, w, h) {
-    var imageData = Canvas.context.getImageData(Canvas.getX(x), Canvas.getY(y), Canvas.getX(w), Canvas.getY(h));
-    var newCanvas = document.createElement("canvas");
-    newCanvas.width  = imageData.width;
-    newCanvas.height = imageData.height;
-    newCanvas.getContext("2d").putImageData(imageData, 0, 0);
-    return newCanvas;
+    if(!Canvas.configured) {
+      console.warn("KAPhy Warning - You must use Canvas.configure(); before you can draw!");
+      return;
+    }
+    
+    if(arguments.length === 4) {
+      var imageData = Canvas.context.getImageData(Canvas.getX(x), Canvas.getY(y), Canvas.getX(w), Canvas.getY(h));
+      var newCanvas = document.createElement("canvas");
+      newCanvas.width  = imageData.width;
+      newCanvas.height = imageData.height;
+      newCanvas.getContext("2d").putImageData(imageData, 0, 0);
+      return newCanvas;
+    }
+    else if(arguments.length === 2) {
+      var dataAtPoint = Canvas.context.getImageData(Canvas.getX(x), Canvas.getY(y), 1, 1);
+      return {
+        r: dataAtPoint.data[0],
+        g: dataAtPoint.data[1],
+        b: dataAtPoint.data[2],
+        a: dataAtPoint.data[3] * 255
+      };
+    }
   };
 }
