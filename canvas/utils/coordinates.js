fa90a1b84009229d16c6f86f0b57e5f877789980
TemporalFuzz <vanxmax@gmail.com>
2017-07-29 14:06:48 -0700

    Update coordinates.js

--- canvas/utils/coordinates.js
+++ canvas/utils/coordinates.js
@@ -8,3 +8,13 @@ if(!Canvas.getY || KAPhy.version !== KAPhy.current) {
     return y * Canvas.element.height/Canvas.relHeight;
   };
 }
+if(!Canvas.xFromConverted || KAPhy.version !== KAPhy.current) {
+  Canvas.xFromConverted = function(x) {
+    return x * Canvas.relWidth/Canvas.element.width;
+  }
+}
+if(!Canvas.yFromConverted || KAPhy.version !== KAPhy.current) {
+  Canvas.yFromConverted = function(y) {
+    return y * Canvas.relHeight/Canvas.element.height;
+  }
+}
