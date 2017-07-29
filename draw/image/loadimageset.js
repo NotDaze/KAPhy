4abd0cbbc2c8df2147171b5d7f5270940068778a
TemporalFuzz <vanxmax@gmail.com>
2017-07-28 17:35:03 -0700

    Update loadimageset.js

--- draw/image/loadimageset.js
+++ draw/image/loadimageset.js
@@ -7,14 +7,16 @@ if(!Draw.loadImageSet) {
     
     var imagesLoaded = 0;
     var imagesToLoad = arguments;
+    var loadedImages = [];
     imagesToLoad.pop();
     var onFinish = arguments[arguments.length - 1];
     
     for(var i = 0; i < imagesToLoad.length; i++) {
       Draw.loadImage(imagesToLoad[i], function() {
+        loadedImages.push(this);
         imagesLoaded++;
         if(imagesLoaded >= imagesToLoad.length) {
-          onFinish();
+          onFinish.call(loadedImages);
         }
       });
     }
