b720dd8f26c4c6020093130dc52ef12223624b1a
TemporalFuzz <vanxmax@gmail.com>
2017-07-30 13:07:58 -0700

    Update kaphy.js

--- kaphy.js
+++ kaphy.js
@@ -1,20 +1,21 @@
-var KAPhy = {
-  current: "0.0.4.11",
-  install: function() {
-    if(!KAPhy.installed) {
-      KAPhy.installed = true;
-      KAPhy.version = KAPhy.current;
+if (!KAPhy)
+  var KAPhy = {
+    current: "0.0.4.11",
+    install: function() {
+      if (!KAPhy.installed) {
+        KAPhy.installed = true;
+        KAPhy.version = KAPhy.current;
+      } else if (KAPhy.version !== KAPhy.current) {
+        console.log("KAPhy is updating.");
+      }
+    },
+    finishUpdate: function() {
+      if (KAPhy.version !== KAPhy.current) {
+        KAPhy.version = KAPhy.current;
+        console.log("KAPhy update finished!");
+      }
+      console.log("KAPhy loaded!");
     }
-    else if(KAPhy.version !== KAPhy.current){
-      console.log("KAPhy is updating.");
-    }
-  },
-  finishUpdate: function() {
-    if(KAPhy.version !== KAPhy.current) {
-      KAPhy.version = KAPhy.current;
-      console.log("KAPhy update finished!");
-    }
-    console.log("KAPhy loaded!");
-  }
-};
+  };
+}
 KAPhy.install();
