7ec1052bb0cabf9bde7fc9279736c6d3612577c8
TemporalFuzz <vanxmax@gmail.com>
2017-07-30 17:48:54 -0700

    Create ellipsesection.js

---
+++ draw/shapes/ellipsesection.js
@@ -0,0 +1,45 @@
+if(!Draw.ellipseSection || KAPhy.version !== KAPhy.current) {
+  Draw.ellipseSection = function(x, y, w, h, start, stop) {
+    if(!Canvas.configured) {
+      console.warn("KAPhy Warning - You must use Canvas.configure(); before you can draw!");
+      return;
+    }
+    
+    if(Draw.shapeOn) {
+      console.warn("KAPhy Warning - You can't use other shape commands when in shape mode.");
+      return;
+    }
+    
+    Canvas.context.beginPath();
+    
+    var realX = x,
+        realY = y,
+        realW = w,
+        realH = h;
+    
+    switch(Draw.currentEllipseMode) {
+      case CORNER: 
+        realX = x - w/2;
+        realY = y - h/2; break;
+      case CORNERS:
+        realX = (x + w)/2;
+        realY = (y + h)/2;
+        realW = w - x;
+        realH = h - y; break;
+      case RADIUS: 
+        realW = w * 2;
+        realH = h * 2; break;
+    }
+    
+    if(Canvas.configured) {
+      realX = Canvas.getX(realX);
+      realY = Canvas.getY(realY);
+      realW = Canvas.getX(realW);
+      realH = Canvas.getY(realH);
+    }
+    
+    Canvas.context.ellipse(realX, realY, realW, realH, 0, start, stop);
+    Canvas.context.fill();
+    Canvas.context.stroke();
+  };
+}
