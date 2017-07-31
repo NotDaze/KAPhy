9d05b91b62d4297cfc1697403b5db8d195177cf3
TemporalFuzz <vanxmax@gmail.com>
2017-07-30 21:50:59 -0700

    Create pushmatrix.js

---
+++ draw/transform/pushmatrix.js
@@ -0,0 +1,34 @@
+if(!Draw.pushMatrix) {
+  Draw.pushMatrix = function() {
+    if(!Canvas.configured) {
+      console.warn("KAPhy Warning - You must use Canvas.configure(); before you can draw!");
+      return;
+    }
+    
+    var oldFillStyle    = Canvas.context.fillStyle;
+    var oldStrokeStyle  = Canvas.context.strokeStyle;
+    var oldLineWidth    = Canvas.context.lineWidth;
+    var oldLineCap      = Canvas.context.lineCap;
+    var oldLineJoin     = Canvas.context.lineJoin;
+    var oldGlobalAlpha  = Canvas.context.globalAlpha;
+    var oldMiterLimit   = Canvas.context.miterLimit;
+    var oldFont         = Canvas.context.font;
+    var oldTextAlign    = Canvas.context.textAlign;
+    var oldTextBaseline = Canvas.context.textBaseline;
+    var oldSmoothing    = Canvas.context.imageSmoothingEnabled;
+    
+    Canvas.context.restore();
+    
+    Canvas.context.fillStyle    = oldFillStyle;
+    Canvas.context.strokeStyle  = oldStrokeStyle;
+    Canvas.context.lineWidth    = oldLineWidth;
+    Canvas.context.lineCap      = oldLineCap;
+    Canvas.context.lineJoin     = oldLineJoin;
+    Canvas.context.globalAlpha  = oldGlobalAlpha;
+    Canvas.context.miterLimit   = oldMiterLimit;
+    Canvas.context.font         = oldFont;
+    Canvas.context.textAlign    = oldTextAlign;
+    Canvas.context.textBaseline = oldTextBaseline;
+    Canvas.context.imageSmoothingEnabled = oldSmoothing;
+  };
+}
