4884b52cea7d8d39ad535ca713ae46494b094e27
TemporalFuzz <vanxmax@gmail.com>
2017-07-31 13:27:53 -0700

    Update textalign.js

--- draw/settings/textalign.js
+++ draw/settings/textalign.js
@@ -1,14 +1,15 @@
 if(!Draw.textAlign) {
-  Draw.textAlign = function(newAlign) {
+  Draw.textAlign = function(newAlign, yAlign) {
     if(!Canvas.configured) {
       console.warn("KAPhy Warning - You must use Canvas.configure(); before you can draw!");
       return;
     }
     
-    if(arguments.length !== 1) {
-      console.warn("KAPhy Warning - Draw.textAlign() takes 1 argument.");
+    if(arguments.length > 2) {
+      console.warn("KAPhy Warning - Draw.textAlign() takes 0 - 2 arguments.");
     }
     
-    Canvas.context.textAlign = newAlign;
+    Canvas.context.textAlign = newAlign || "left";
+    if(yAlign) Canvas.context.textBaseline = yAlign;
   };
 }
