bc2f4a00d31e41859fb889946c70f5369c2fa586
TemporalFuzz <vanxmax@gmail.com>
2017-07-30 22:02:36 -0700

    Update popmatrix.js

--- draw/transform/popmatrix.js
+++ draw/transform/popmatrix.js
@@ -5,6 +5,30 @@ if(!Draw.popMatrix) {
       return;
     }
     
-    Canvas.context.save();
+    var oldFillStyle    = Canvas.context.fillStyle;
+    var oldStrokeStyle  = Canvas.context.strokeStyle;
+    var oldLineWidth    = Canvas.context.lineWidth;
+    var oldLineCap      = Canvas.context.lineCap;
+    var oldLineJoin     = Canvas.context.lineJoin;
+    var oldGlobalAlpha  = Canvas.context.globalAlpha;
+    var oldMiterLimit   = Canvas.context.miterLimit;
+    var oldFont         = Canvas.context.font;
+    var oldTextAlign    = Canvas.context.textAlign;
+    var oldTextBaseline = Canvas.context.textBaseline;
+    var oldSmoothing    = Canvas.context.imageSmoothingEnabled;
+    
+    Canvas.context.restore();
+    
+    Canvas.context.fillStyle    = oldFillStyle;
+    Canvas.context.strokeStyle  = oldStrokeStyle;
+    Canvas.context.lineWidth    = oldLineWidth;
+    Canvas.context.lineCap      = oldLineCap;
+    Canvas.context.lineJoin     = oldLineJoin;
+    Canvas.context.globalAlpha  = oldGlobalAlpha;
+    Canvas.context.miterLimit   = oldMiterLimit;
+    Canvas.context.font         = oldFont;
+    Canvas.context.textAlign    = oldTextAlign;
+    Canvas.context.textBaseline = oldTextBaseline;
+    Canvas.context.imageSmoothingEnabled = oldSmoothing;
   };
 }
