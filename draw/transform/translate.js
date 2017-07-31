38fe05a89272967d241508ff319d1a1f0582b320
TemporalFuzz <vanxmax@gmail.com>
2017-07-30 20:24:14 -0700

    Create translate.js

---
+++ draw/transform/translate.js
@@ -0,0 +1,20 @@
+if(!Draw.translate) {
+  Draw.translate = function(x, y) {
+    if(!Canvas.configured) {
+      console.warn("KAPhy Warning - You must use Canvas.configure(); before you can draw!");
+      return;
+    }
+    
+    if(Draw.shapeOn) {
+      console.warn("KAPhy Warning - You can't use transformation commands when in shape mode.");
+      return;
+    }
+    
+    if(arguments.length === 0 || arguments.length > 2) {
+      console.warn("KAPhy Warning - Draw.translate() takes 1 - 2 arguments.");
+      return;
+    }
+    
+    Canvas.context.transform(1, 0, 0, 1, x, y || 0);
+  };
+}
