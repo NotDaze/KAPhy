fcaa3a09319835fd22b5ce4f9e2607d5f4a4a85d
TemporalFuzz <vanxmax@gmail.com>
2017-07-30 19:48:30 -0700

    Create strokejoin.js

---
+++ draw/settings/strokejoin.js
@@ -0,0 +1,12 @@
+if(!Draw.strokeJoin) {
+  Draw.strokeJoin = function(newJoin) {
+    if(!Canvas.configured) {
+      console.warn("KAPhy Warning - You must use Canvas.configure(); before you can draw!");
+      return;
+    }
+    
+    if(newJoin === MITER || newJoin === ROUND || newJoin === BEVEL) {
+      Canvas.context.lineCap = newJoin;
+    }
+  };
+}
