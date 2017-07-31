72af5a834fd8918f3da6ea88c52539df283eb252
TemporalFuzz <vanxmax@gmail.com>
2017-07-31 15:29:09 -0700

    Update init.js

--- draw/init.js
+++ draw/init.js
@@ -10,6 +10,18 @@ if(!Draw || KAPhy.version !== KAPhy.current) {
   var SQUARE = "butt";
   var PROJECT = "square";
   
+  var START = "start";
+  var END = "end";
+  var LEFT = "left";
+  var RIGHT = "right";
+  var CENTER = "center";
+  
+  var TOP = "top";
+  var BOTTOM = "bottom";
+  var MIDDLE = "middle";
+  var ALPHABETIC = "alphabetic";
+  var HANGING = "hanging";
+  
   var Draw = {
     currentRectMode: "CORNER",
     currentImageMode: "CORNER",
