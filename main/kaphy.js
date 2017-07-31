f4fb0a42c894169d3eca96bfd3812d816ad1cc1e
TemporalFuzz <vanxmax@gmail.com>
2017-07-30 17:01:22 -0700

    Update kaphy.js

--- main/kaphy.js
+++ main/kaphy.js
@@ -2,34 +2,17 @@ if (!KAPhy) {
   var KAPhy = {
     current: "0.0.5",
     install: function() {
-      KAPhy.version = window.localStorage.getItem("kaphyversion");
-      
-      if(!KAPhy.localStorageUsable()) {
-        console.log("KAPhy is updating or installing.");
-        return;
-      }
       if (KAPhy.version !== KAPhy.current) {
-        console.log("KAPhy is updating or installing.");
+        console.log("KAPhy is updating, installing, or rebuilding.");
       }
     },
     finishUpdate: function() {
       if (KAPhy.version !== KAPhy.current) {
         KAPhy.version = KAPhy.current;
-        window.localStorage.setItem("kaphyversion", KAPhy.current);
-        console.log("KAPhy update finished!");
+        console.log("KAPhy build finished!");
       }
       console.log("KAPhy loaded!");
     },
-    localStorageUsable: function() {
-      try {
-        var storage = window.localStorage,
-        x = '__local_storage_test_this_will_be_overridden_do_not_use_sincerely_temporalfuzz__';
-        storage.setItem(x, x);
-        storage.removeItem(x);
-        return true;
-      }
-      catch(e) { return false; }
-    }
   };
 }
 KAPhy.install();
